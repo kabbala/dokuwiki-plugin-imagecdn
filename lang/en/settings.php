8d2f61ae333b0165db404f34eef9c0664d50ad8b
kabbala <kabbala@users.noreply.github.com>
2020-12-01 04:13:55 +0900

    add language file(en, ko)

---
+++ lang/en/settings.php
@@ -0,0 +1,4 @@
+<?php
+$lang['imagecdn_url'] = 'Image CDN address.(include HTTP/HTTPS protocol)';
+$lang['imagecdn_url_suffix'] = 'Query strings for Image CDN or DokuWiki Media Manager. (seperate with &)';
+$lang['use_fetch'] = 'Will you use Fetch.php?';
---
+++ lang/ko/settings.php
@@ -0,0 +1,4 @@
+<?php
+$lang['imagecdn_url'] = '이미지 CDN 주소. (HTTP/HTTPS 프로토콜 포함)';
+$lang['imagecdn_url_suffix'] = '이미지 CDN 또는 도쿠위키 미디어 관리자에서 사용할 옵션 (&로 구분)';
+$lang['use_fetch'] = '도쿠위키의 fetch.php 기능 사용 여부';
